d58ce974c36abda0b550a456d7680edd7e1aa74c
xAbdx <67425264+xAbdx@users.noreply.github.com>
2021-01-14 14:00:20 +0200

    added login form with auth and guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ARowComponent } from './a-table/a-row/a-row.component';
 import { FormsModule } from '@angular/forms';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './parent/child/child.component';
+import { LoginComponent } from './login/login.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -16,10 +19,13 @@ import { ChildComponent } from './parent/child/child.component';
     ARowComponent,
     ParentComponent,
     ChildComponent,
+    LoginComponent,
+    ProfilePageComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  loginUser(username, password){
+    //
+    //
+  }
+
+  isLoggedIn() {
+    return
+  }
+
+  constructor() { }
+}
